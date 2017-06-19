42741abd29c188f2e8f2a8ef3694604469d9ccd8
chentsuhsi <chen.tsu.hsi@gmail.com>
2017-06-18 21:35:13 -0400

    Update 2017/06/17 14:20a

--- rndQuotes/scripts/script.js
+++ rndQuotes/scripts/script.js
@@ -7,11 +7,20 @@ $(document).ready(function() {
         $.getJSON(
             "http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
             function (data) {
+                var html = "";
                 data = JSON.parse(data);
                 quoteAuthor = data.quoteAuthor;
                 quoteTxt = data.quoteText;
 
-                $(".quote").html(JSON.stringify(data}
+                html += '<div class="quote-text">' +
+                        '    <i class="fa fa-quote-left" aria-hidden="true"></i>'+
+                         quoteTxt +
+                         '<i class="fa fa-quote-right" aria-hidden="true"></i>'+
+                         '</div>';
+                html += '<div class="quote-author">' + quoteTxt + '</div>';
+
+                $(".quote").html(html);
+            }
         )
         // Only change code above this line.
     });
